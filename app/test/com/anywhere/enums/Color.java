c06537ce50e2575238f8ed355f4a116c9128456b
anywhere <jinyifeng5969@163.com>
2013-06-25 23:05:34 +0800

    增加枚举例子

---
+++ app/test/com/anywhere/enums/Color.java
@@ -0,0 +1,10 @@
+package com.anywhere.enums;
+
+public interface Color {
+    
+    static int RED = 1;
+    
+    static int GREEN = 2;
+    
+    static int BLUE = 3;
+}
---
+++ app/test/com/anywhere/enums/EnumDemo.java
@@ -0,0 +1,15 @@
+package com.anywhere.enums;
+
+public class EnumDemo {
+    
+    private enum Color {
+        red, blue, green
+    }// there is not a ";"
+    
+    public static void main(String[] args) {
+        for (Color s : Color.values()) {
+            // enum的values()返回一个数组，这里就是Seasons[]
+            System.out.println(s);
+        }
+    }
+}
